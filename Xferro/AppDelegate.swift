f411e98a78587dd50ce90bfa2c7aa984952eafe3
Taha Bebek <tsbebek@gmail.com>
2025-01-08 21:26:26 -0800

    Initial Commit

---
+++ Xferro/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Xferro
+//
+//  Created by Taha Bebek on 1/8/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Xferro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Xferro
+//
+//  Created by Taha Bebek on 1/8/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
