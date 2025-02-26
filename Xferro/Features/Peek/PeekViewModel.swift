85fde8bf2176c498ed7a5e50f6af16905850f8bb
Taha Bebek <tsbebek@gmail.com>
2025-02-26 08:27:49 -0500

    empty hunks

--- Xferro/Features/Peek/PeekViewModel.swift
+++ Xferro/Features/Peek/PeekViewModel.swift
@@ -91,6 +91,8 @@ import Observation
                     }
                 }
                 self.hunks = hunks
+            } else {
+                self.hunks = []
             }
         }
     }
