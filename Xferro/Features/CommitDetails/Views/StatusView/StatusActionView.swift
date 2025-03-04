a45e27d7de8c9db7ca98a25fc48e5dd00931fbf8
Taha Bebek <tsbebek@gmail.com>
2025-03-04 06:28:07 -0500

    don't show progress

--- Xferro/Features/CommitDetails/Views/StatusView/StatusActionView.swift
+++ Xferro/Features/CommitDetails/Views/StatusView/StatusActionView.swift
@@ -12,73 +12,54 @@ struct StatusActionView: View {
     @FocusState private var isTextFieldFocused: Bool
     @State private var horizontalAlignment: HorizontalAlignment = .leading
     @State private var verticalAlignment: VerticalAlignment = .top
-    @State private var showProgress: Bool = false
-
+    
     let commitSummaryIsEmptyOrWhitespace: Bool
     let stagedDeltaInfosIsEmpty: Bool
     let hasChanges: Bool
     let onCommitTapped: () -> Void
     let onBoxActionTapped: (StatusActionButtonsView.BoxAction) async -> Void
-
+    
     var body: some View {
-        Group {
-            if showProgress {
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        ProgressView()
-                        Spacer()
-                    }
-                    Spacer()
+        
+        VStack {
+            HStack {
+                Form {
+                    TextField(
+                        "Summary",
+                        text: $commitSummary,
+                        prompt: Text("Summary for commit, amend or stash"),
+                        axis: .vertical
+                    )
+                    .focused($isTextFieldFocused)
+                    .textFieldStyle(.roundedBorder)
                 }
-            } else {
-                VStack {
-                    HStack {
-                        Form {
-                            TextField(
-                                "Summary",
-                                text: $commitSummary,
-                                prompt: Text("Summary for commit, amend or stash"),
-                                axis: .vertical
-                            )
-                            .focused($isTextFieldFocused)
-                            .textFieldStyle(.roundedBorder)
-                        }
-                        XFerroButton(
-                            title: "Commit",
-                            disabled: commitSummaryIsEmptyOrWhitespace || stagedDeltaInfosIsEmpty || !hasChanges,
-                            dangerous: false,
-                            isProminent: true,
-                            onTap: {
-                                onCommitTapped()
-                                isTextFieldFocused = false
-                            }
-                        )
+                XFerroButton(
+                    title: "Commit",
+                    disabled: commitSummaryIsEmptyOrWhitespace || stagedDeltaInfosIsEmpty || !hasChanges,
+                    dangerous: false,
+                    isProminent: true,
+                    onTap: {
+                        onCommitTapped()
+                        isTextFieldFocused = false
                     }
-                    .padding(.bottom, StatusView.actionBoxBottomPadding)
-                    AnyLayout(FlowLayout(alignment:.init(horizontal: horizontalAlignment, vertical: verticalAlignment))) {
-                        StatusActionButtonsView(
-                            hasChanges: hasChanges,
-                            stagedDeltaInfosIsEmpty: stagedDeltaInfosIsEmpty,
-                            commitSummaryIsEmptyOrWhitespace: commitSummaryIsEmptyOrWhitespace,
-                            onTap: { action in
-                                isTextFieldFocused = false
-                                showProgress = true
-                                Task {
-                                    await onBoxActionTapped(action)
-                                    await MainActor.run {
-                                        showProgress = false
-                                    }
-                                }
-                            }
-                        )
-                    }
-                    .animation(.default, value: horizontalAlignment)
-                    .animation(.default, value: verticalAlignment)
-                    .animation(.default, value: showProgress)
-                }
+                )
             }
+            .padding(.bottom, StatusView.actionBoxBottomPadding)
+            AnyLayout(FlowLayout(alignment:.init(horizontal: horizontalAlignment, vertical: verticalAlignment))) {
+                StatusActionButtonsView(
+                    hasChanges: hasChanges,
+                    stagedDeltaInfosIsEmpty: stagedDeltaInfosIsEmpty,
+                    commitSummaryIsEmptyOrWhitespace: commitSummaryIsEmptyOrWhitespace,
+                    onTap: { action in
+                        isTextFieldFocused = false
+                        Task {
+                            await onBoxActionTapped(action)
+                        }
+                    }
+                )
+            }
+            .animation(.default, value: horizontalAlignment)
+            .animation(.default, value: verticalAlignment)
         }
         .onAppear {
             isTextFieldFocused = true
