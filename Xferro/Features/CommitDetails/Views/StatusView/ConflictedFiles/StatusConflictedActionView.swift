8d07284571b6642bc62100815c008326d4544eca
Taha Bebek <tsbebek@gmail.com>
2025-03-26 17:04:15 -0500

    padding

--- Xferro/Features/CommitDetails/Views/StatusView/ConflictedFiles/StatusConflictedActionView.swift
+++ Xferro/Features/CommitDetails/Views/StatusView/ConflictedFiles/StatusConflictedActionView.swift
@@ -18,7 +18,8 @@ enum ConflictType: String {
 }
 
 struct StatusConflictedActionView: View {
-
+    @State private var horizontalAlignment: HorizontalAlignment = .leading
+    @State private var verticalAlignment: VerticalAlignment = .top
     let conflictType: ConflictType
 
     let onContinueMergeTapped: () -> Void
@@ -27,47 +28,53 @@ struct StatusConflictedActionView: View {
     let onAbortRebaseTapped: () -> Void
 
     var body: some View {
-        HStack {
-            switch conflictType {
-            case .merge, .interactiveRebase:
-                XFButton<Void>(
-                    title: "Continue Merge",
-                    info: XFButtonInfo(info: InfoTexts.continueMerge),
-                    onTap: {
-                        onContinueMergeTapped()
-                    }
-                )
-                XFButton<Void>(
-                    title: "Abort Merge",
-                    info: XFButtonInfo(info: InfoTexts.abortMerge),
-                    onTap: {
-                        onAbortMergeTapped()
-                    }
-                )
-            case .rebase:
-                XFButton<Void>(
-                    title: "Continue Rebase",
-                    info: XFButtonInfo(info: InfoTexts.continueMerge),
-                    onTap: {
-                        onContinueRebaseTapped()
-                    }
-                )
-                XFButton<Void>(
-                    title: "Abort Rebase",
-                    info: XFButtonInfo(info: InfoTexts.abortMerge),
-                    onTap: {
-                        onAbortRebaseTapped()
-                    }
-                )
-            case .cherryPick:
-                fatalError(.unimplemented)
-            case .revert:
-                fatalError(.unimplemented)
-            case .bisect:
-                fatalError(.unimplemented)
-            case .squash:
-                fatalError(.unimplemented)
+        VStack {
+            AnyLayout(FlowLayout(alignment:.init(
+                horizontal: horizontalAlignment,
+                vertical: verticalAlignment))) {
+                switch conflictType {
+                case .merge, .interactiveRebase:
+                    XFButton<Void>(
+                        title: "Continue Merge",
+                        info: XFButtonInfo(info: InfoTexts.continueMerge),
+                        onTap: {
+                            onContinueMergeTapped()
+                        }
+                    )
+                    XFButton<Void>(
+                        title: "Abort Merge",
+                        info: XFButtonInfo(info: InfoTexts.abortMerge),
+                        onTap: {
+                            onAbortMergeTapped()
+                        }
+                    )
+                case .rebase:
+                    XFButton<Void>(
+                        title: "Continue Rebase",
+                        info: XFButtonInfo(info: InfoTexts.continueMerge),
+                        onTap: {
+                            onContinueRebaseTapped()
+                        }
+                    )
+                    XFButton<Void>(
+                        title: "Abort Rebase",
+                        info: XFButtonInfo(info: InfoTexts.abortMerge),
+                        onTap: {
+                            onAbortRebaseTapped()
+                        }
+                    )
+                case .cherryPick:
+                    fatalError(.unimplemented)
+                case .revert:
+                    fatalError(.unimplemented)
+                case .bisect:
+                    fatalError(.unimplemented)
+                case .squash:
+                    fatalError(.unimplemented)
+                }
             }
+            .animation(.default, value: horizontalAlignment)
+            .animation(.default, value: verticalAlignment)
         }
     }
 }
