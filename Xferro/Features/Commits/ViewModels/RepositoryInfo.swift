4c89377f07d115f6d1e862537e872130daca0f51
Taha Bebek <tsbebek@gmail.com>
2025-03-26 11:21:26 -0500

    change warning

--- Xferro/Features/Commits/ViewModels/RepositoryInfo.swift
+++ Xferro/Features/Commits/ViewModels/RepositoryInfo.swift
@@ -282,7 +282,7 @@ extension RepositoryInfo: Equatable {
 // MARK: Static
 extension RepositoryInfo {
     static let commitCountLimit: Int = 10
-#warning("check if git and workdir debounces are in sync, maybe do not use status if there is a risk of race condition")
+#warning("check if first and workdir debounces are in sync, maybe do not use status if there is a risk of race conditions")
     static let workDirDebounce = 5
 
     static func taskQueueID(path: String) -> String
